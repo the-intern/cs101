34f5da8cf3b9e7980b50798c629b447fab0c89b3
the-intern <ostclasses@gmail.com>
2022-08-10 15:25:13 -0500

    quiz testing embeds

---
+++ book/part-i/quiz-i/data.py
@@ -0,0 +1,19 @@
+query = [
+    {
+    "question": "What is the wind velocity of a coconut-laden sparrow?",
+    "type": "multiple_choice",
+    "answers": [
+        {
+            "code": "146 knots",
+            "correct": False
+        },
+        {
+            "code": "European or African sparrow?",
+            "correct": True
+        },
+        {
+            "code": "Blue",
+            "correct": False
+        }
+    ]
+}]
