d01384295eb0146fd02a157ab7f082a8c5948ed3
the-intern <ostclasses@gmail.com>
2022-10-07 18:17:31 -0500

    working on vids and part iii procedures pages

---
+++ book/part-iii/qsource.py
@@ -0,0 +1,3 @@
+src = 'https://raw.githubusercontent.com/jmshea/Foundations-of-Data-Science-with-Python/main/questions/ch3.json'
+
+srcii = 'https://raw.githubusercontent.com/jmshea/jupyterquiz/main/examples/questions.json'
